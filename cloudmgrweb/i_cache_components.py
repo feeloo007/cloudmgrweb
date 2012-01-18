9ccb69cc70264fed8420b7c37f2413ac0df2ddfd
Philippe GONCALVES <github@feeloo007.com>
2012-01-18 07:48:26 -0800

    Début d'une nouvelle implémentation gérant les flux comet pour les mises à jour de clients à clients

---
+++ cloudmgrweb/i_cache_components.py
@@ -0,0 +1,82 @@
+# -*- coding: UTF-8 -*-
+from __future__ 	import with_statement
+from nagare		import presentation, var
+from ajax_x_components  import XComponentsUpdates
+from pprint		import pprint
+
+class ICacheComponents( object ):
+
+   def __init__( self, cache_components = None ):
+      if not cache_components:
+         self._cache_components = {}
+      else:
+         self._cache_components = cache_components.cache_components
+
+   def get_cache_components( self ):
+     return self._cache_components
+
+   cache_components = property( get_cache_components )
+
+   def set_knowndiv_for( self, event_name, cp_div, appcode = '*', aera = '*', env = '*', appcomp = '*' ):
+
+      self.cache_components.setdefault(
+         event_name,
+         {}
+      ).setdefault(
+         appcode,
+         {}
+      ).setdefault(
+         aera,
+         {}
+      ).setdefault(
+         env,
+         {}
+      ).setdefault( 
+          appcomp, 
+          {}                                  
+      ).setdefault( 
+           cp_div.uid,
+           lambda: cp_div.le_get_knowndiv()
+      )
+
+
+   def get_l_known_div_for_change( self, event_name, appcode = '*', aera = '*', env = '*', appcomp = '*' ):
+      return [ le() for le in self.cache_components.get(
+            event_name
+         ).get(
+           appcode
+         ).get(
+           aera
+         ).get(
+           env
+         ).get( appcomp ).values()
+      ]
+
+      return []
+
+class FormRefreshOnComet( ICacheComponents ):
+   def __init__( self, cache_components = None ):
+      ICacheComponents.__init__( self, cache_components = cache_components )
+
+@presentation.render_for( FormRefreshOnComet )
+def render(self, h, *args):
+
+   v_event_name = var.Var()
+   v_appcode 	= var.Var()
+   v_aera 	= var.Var()
+   v_env 	= var.Var()
+   v_appcomp 	= var.Var()
+   
+   def fake():
+      pass
+
+   with h.form( id = 'refresh_on_comet_event' ):
+         #h << h.input( type = 'submit', class_ = 'submit', style = 'display: none ;' ).action( fake )
+         h << h.input( type = 'submit', class_ = 'submit', style = 'display: none ;' ).action( XComponentsUpdates( le_l_knowndiv = lambda: self.get_l_known_div_for_change( v_event_name(), appcode = v_appcode(), aera = v_aera(), env = v_env(), appcomp = v_appcomp() ), update_himself = True, action = lambda: fake() ) )
+         h << h.input( type = 'hidden', class_ = 'event_name' ).action( v_event_name )
+         h << h.input( type = 'hidden', class_ = 'appcode' ).action( v_appcode )
+         h << h.input( type = 'hidden', class_ = 'aera' ).action( v_aera )
+         h << h.input( type = 'hidden', class_ = 'env' ).action( v_env )
+         h << h.input( type = 'hidden', class_ = 'appcomp' ).action( v_appcomp )
+
+   return h.root
--- static/cloudmgrweb_comet.js
+++ static/cloudmgrweb_comet.js
@@ -1,32 +1,63 @@
+//function cloudmgrweb_comet_refresh( refresh_identification ) {
+//
+//   var l_tokens = refresh_identification.split( " " ) ;
+//   var refresh_on_name = l_tokens.shift() ;
+//
+//   for ( i = 0; i < document.getElementsByName( refresh_on_name ).length; i++ ) {
+//      var l_class = document.getElementsByName( refresh_on_name ).item( i ).className.split( ' ' ) ;
+//
+//      // Vérifions si le composant doit être notifié quelque soit les paramètres supplémentaires
+//      if ( ( l_class.length == 1 ) && ( l_class[ 0 ] == 'ALL' ) ) {
+//         document.getElementsByName( refresh_on_name ).item( i )[ 2 ].click() ;
+//      }
+//
+//      // Vérifions si le composant possède strictement les mêmes classes
+//      l_class_to_search	= l_tokens.sort().slice( 0 ) ;
+//      l_class_found 	= l_class.sort().slice( 0 ) ;
+//      for ( j = 0 ; j < l_class_to_search.length ; j++ ) {
+//         for ( h = 0 ; h < l_class_found.length ; h++ ) {
+//            if ( l_class_to_search[ j ] == l_class_found[ h ] ) {
+//               l_class_to_search.splice( j, 1 ) ;
+//               delete l_class_found.splice( h, 1 ) ;
+//               j-- ;
+//               break ;
+//            }
+//         }
+//      } 
+//      if ( l_class_to_search.length == 0 ) {
+//         console.log( 'click' )
+//         document.getElementsByName( refresh_on_name ).item( i )[ 2 ].click() ;
+//      }
+//   }
+//}
+
 function cloudmgrweb_comet_refresh( refresh_identification ) {
+   console.log( refresh_identification )
+
+   var form_id	 	= "refresh_on_comet_event" ;
+   var event_name 	= "event_name" ;
+   var l_params_name 	= [ "appcode", "aera", "env", "appcomp" ] ;
+   var formsub_name 	= "submit" ;
 
    var l_tokens = refresh_identification.split( " " ) ;
-   var refresh_on_name = l_tokens.shift() ;
+   
+   document.getElementById( form_id ).getElementsByClassName( event_name )[ 0 ].value = l_tokens.shift() ;
 
-   for ( i = 0; i < document.getElementsByName( refresh_on_name ).length; i++ ) {
-      var l_class = document.getElementsByName( refresh_on_name ).item( i ).className.split( ' ' ) ;
+   var d_params = {} ;
+   for ( i = 0 ; i < l_tokens.length; i++ ) {
+      l_param_value = l_tokens[ i ].split( ":" ) ;
+      d_params[ l_param_value[ 0 ] ] = l_param_value[ 1 ] ; 
+   }
 
-      // Vérifions si le composant doit être notifié quelque soit les paramètres supplémentaires
-      if ( ( l_class.length == 1 ) && ( l_class[ 0 ] == 'ALL' ) ) {
-         document.getElementsByName( refresh_on_name ).item( i )[ 2 ].click() ;
+   for ( i = 0 ; i < l_params_name.length; i++ ) { 
+      if ( d_params[ l_params_name[ i ] ] != null ) { 
+         document.getElementById( form_id ).getElementsByClassName( l_params_name[ i ] )[ 0 ].value = d_params[ l_params_name[ i ] ] ;
       }
-
-      // Vérifions si le composant possède strictement les mêmes classes
-      l_class_to_search	= l_tokens.sort().slice( 0 ) ;
-      l_class_found 	= l_class.sort().slice( 0 ) ;
-      for ( j = 0 ; j < l_class_to_search.length ; j++ ) {
-         for ( h = 0 ; h < l_class_found.length ; h++ ) {
-            if ( l_class_to_search[ j ] == l_class_found[ h ] ) {
-               l_class_to_search.splice( j, 1 ) ;
-               delete l_class_found.splice( h, 1 ) ;
-               j-- ;
-               break ;
-            }
-         }
-      } 
-      if ( l_class_to_search.length == 0 ) {
-         console.log( 'click' )
-         document.getElementsByName( refresh_on_name ).item( i )[ 2 ].click() ;
+      else {
+         document.getElementById( form_id ).getElementsByClassName( l_params_name[ i ] )[ 0 ].value = '*' ;
       }
    }
+
+   document.getElementById( form_id ).getElementsByClassName( formsub_name )[ 0 ].click() ;
+
 }
