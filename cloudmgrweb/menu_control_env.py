d95d3f69933534e6a0710749f7ab7c6216798126
Philippe GONCALVES <github@feeloo007.com>
2012-01-19 06:49:50 -0800

    Ajout de getters pour les composants manipulés

--- cloudmgrweb/menu_control_env.py
+++ cloudmgrweb/menu_control_env.py
@@ -13,6 +13,8 @@ from i_cache_components                         import ICacheComponents
 # Mise en place d'un DOM pour la gestion comet
 from i_dom                                      import IDom
 
+from pprint					import pprint
+
 ###########################
 # Vision des zones
 ###########################
@@ -30,24 +32,45 @@ class MenuControlEnv( ICloudMgrResolvers, IAppcodeGetters, IEnvGetters, ICacheCo
       self._cp_counter_by_appcode_by_env = component.Component( CounterServers( le_appcode_provider = lambda: self.appcode, le_env_provider = lambda: self.env, resolvers = self, cache_components = self, dom_father = self, dom_complement_element_name = 'by_appcode_by_env' ) )
       self._cp_counter_by_env = component.Component( CounterServers( le_env_provider = lambda: self.env, resolvers = self, cache_components = self, dom_father = self, dom_complement_element_name = 'by_appcode' ) )
 
+   def get_cp_counter_by_appcode_by_env( self ):
+      return self._cp_counter_by_appcode_by_env
+   cp_counter_by_appcode_by_env = property( get_cp_counter_by_appcode_by_env )
+
+   def get_cp_counter_by_env( self ):
+      return self._cp_counter_by_env
+   cp_counter_by_env = property( get_cp_counter_by_env ) 
+
 
 @presentation.render_for( MenuControlEnv )
 def render(self, h, comp, *args):
    with self.cloudmap_resolver:
       with h.div( class_='menu_control_env %s' % ( self.env ) ):
         h << h.div( '%s ' % ( self.env_resolver.get_env_desc( self.env ) ), class_ = 'description' )
-        h << h.div( component.Component( KnownDiv( self._cp_counter_by_appcode_by_env ) ), class_ = 'counter_appcomps_struct' )
+        h << h.div( component.Component( KnownDiv( self.cp_counter_by_appcode_by_env ) ), class_ = 'counter_appcomps_struct' )
         h << h.div( ' / ', class_ = 'counter_appcomps_struct separator' )
-        h << h.div( component.Component( KnownDiv( self._cp_counter_by_env ) ), class_ = 'counter_appcomps_struct' )
+        h << h.div( component.Component( KnownDiv( self.cp_counter_by_env ) ), class_ = 'counter_appcomps_struct' )
+
+   # Vérification de la valeur des id
+   pprint( '%s' % MenuControlEnv.__name__ )
+   for event_name, d_event_name in self.cache_components.items():
+      for appcode, d_appcode in d_event_name.items():
+         for aera, d_aera in d_appcode.items():
+            for env, d_env in d_aera.items():
+               for appcomp, d_appcomp in d_env.items():
+                  for dom_name, le in d_appcomp.items():
+                     pprint( '%s %s %s %s %s %s %s' % ( le().id, event_name, appcode, aera, env, appcomp, dom_name ) )
+   print
+
    return h.root
 
 @presentation.render_for( MenuControlEnv, model = '*' )
 def render(self, h, comp, *args):
    with self.cloudmap_resolver:
-      with h.div( class_='menu_control_env %s' % ( self.env ) ):
+      with h.div( class_='menu_control_env SUM' ):
         h << h.div( 'Total ', class_ = 'description' )
-        h << h.div( component.Component( KnownDiv( self._cp_counter_by_appcode_by_env ) ), class_ = 'counter_appcomps_struct' )
+        h << h.div( component.Component( KnownDiv( self.cp_counter_by_appcode_by_env ) ), class_ = 'counter_appcomps_struct' )
         h << h.div( ' / ', class_ = 'counter_appcomps_struct separator' )
-        h << h.div( component.Component( KnownDiv( self._cp_counter_by_env ) ), class_ = 'counter_appcomps_struct' )
+        h << h.div( component.Component( KnownDiv( self.cp_counter_by_env ) ), class_ = 'counter_appcomps_struct' )
+
    return h.root
 
