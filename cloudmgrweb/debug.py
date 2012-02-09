e51073c39fead015df94f7f5ee516e690767a530
Philippe GONCALVES <github@feeloo007.com>
2012-02-09 06:28:38 -0800

    Ajout d'un paramètre à reset_in_dom. Modification des informations sauvegardées

--- cloudmgrweb/debug.py
+++ cloudmgrweb/debug.py
@@ -12,7 +12,14 @@ from pprint                                     import pprint, pformat
 
 from time					import localtime
 
-from colorama					import init, Fore
+from colorama					import init, Fore, Back, Style
+
+from ajax_x_components                          import XComponentsUpdates
+
+
+
+import gc
+import stackless
 
 
 class CloudmgrwebDebug(
@@ -47,12 +54,15 @@ class CloudmgrwebDebug(
 def render(
        self,
        h,
+       comp,
        *args
     ):
 
    # Suppression des précédents fils
    # dans le modèle DOM
-   self.reset_in_dom()
+   self.reset_in_dom(
+           comp
+   )
 
    self.add_event_for_knowndiv( 
       '*', 
@@ -63,19 +73,45 @@ def render(
       appcomp = '*' 
    )
 
-   print( '##################################' )
-   print( '##################################' )
-   print( localtime() )
-   print( '##################################' )
-   print( '##################################' )
-   print( Fore.BLUE + pformat( self._cloudmgrweb.d_dom_tree ) + Fore.RESET )
-   print
-   print
-   print( Fore.CYAN +  pformat( self._cloudmgrweb.d_events ) + Fore.RESET )
-   print
-   print
+   #def print_gc():
+      #gc.collect( 0 )
+      #gc.collect( 1 )
+      #gc.collect( 2 )
+      #print( Fore.CYAN + Back.WHITE + Style.BRIGHT + pformat( stackless.runcount ) + Style.RESET_ALL + Back.RESET + Fore.RESET )
+      #print( Fore.BLACK + Back.WHITE + Style.BRIGHT + pformat( gc.garbage ) + Style.RESET_ALL + Back.RESET + Fore.RESET )
+      #for o in gc.garbage:
+      #   print( Fore.BLUE + Back.WHITE + Style.BRIGHT + pformat( gc.get_referrers( o ) ) + Style.RESET_ALL + Back.RESET + Fore.RESET )
+      #   print( Fore.MAGENTA + Back.WHITE + Style.BRIGHT + pformat( gc.get_referents( o ) ) + Style.RESET_ALL + Back.RESET + Fore.RESET )
+         
+
+   #print( '##################################' )
+   #print( '##################################' )
+   #print( localtime() )
+   #print( '##################################' )
+   #print( '##################################' )
+   #print( Fore.BLUE + pformat( self._cloudmgrweb.d_dom_tree ) + Fore.RESET )
+   #print
+   #print
+   #print( Fore.CYAN +  pformat( self._cloudmgrweb.d_events ) + Fore.RESET )
+   #print
+   #print
+   #print_gc()
 
    h << 'DEBUG IN STDOUT NAGARE-SERVE'
+
+
+   #with h.form():
+   #   h << h.input(
+   #           type = 'submit',
+   #           value= u'Print GC',
+   #          ).action(
+   #             XComponentsUpdates(
+   #                update_himself = True,
+   #                action = print_gc,
+   #             )
+   #          )
+
+
 
    return h.root
 
