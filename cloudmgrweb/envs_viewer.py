33bbc69d9ae410d8353ab74bd4d888c7d168bc84
Philippe GONCALVES <github@feeloo007.com>
2012-02-15 08:20:32 -0800

    Prise en compte de with_cloudmap_resolver et with_cloudmap_resolver_for_render dans envs_viewer.py

--- cloudmgrweb/envs_viewer.py
+++ cloudmgrweb/envs_viewer.py
@@ -5,6 +5,7 @@ from nagare                                     import presentation, component
 from ajax_x_components				import KnownDiv
 from env_viewer					import EnvViewer
 from cloudmgrlib.i_cmgr_resolvers		import ICloudMgrResolvers
+from cloudmgrlib.m_cmgr_cloudmap_resolver       import with_cloudmap_resolver, with_cloudmap_resolver_for_render
 import i_getter
 
 from i_dom_tree                                 import IDomTree
@@ -47,21 +48,24 @@ class EnvsViewer(
          self,
       )
 
-      def create_all_cp_envs_viewer():
-         with self.cloudmap_resolver:
-            d_all_cp_envs = {}
-            for env in self.env_resolver.all_envs:
-               d_all_cp_envs[ env ] = component.Component( 
-                                    EnvViewer( 
-                                       appcode 		= lambda: self.appcode, 
-                                       aera 		= lambda: self.aera, 
-                                       env 		= env, 
-                                       resolvers 	= self, 
-                                       dom_storage 	= self,
-                                       dom_father 	= self,
-                                    ) 
-                                 )
-            return d_all_cp_envs
+      @with_cloudmap_resolver( self )
+      def create_all_cp_envs_viewer(
+             *args,
+             **kwargs
+         ):
+         d_all_cp_envs = {}
+         for env in self.env_resolver.all_envs:
+            d_all_cp_envs[ env ] = component.Component( 
+                                      EnvViewer( 
+                                         appcode 	= lambda: self.appcode, 
+                                         aera 		= lambda: self.aera, 
+                                         env 		= env, 
+                                         resolvers 	= self, 
+                                         dom_storage 	= self,
+                                         dom_father 	= self,
+                                      ) 
+                                   )
+         return d_all_cp_envs
 
       self.create_dynamic_component(
          'all_cp_envs_viewer',
@@ -70,63 +74,63 @@ class EnvsViewer(
 
 
 @presentation.render_for( EnvsViewer )
+@with_cloudmap_resolver_for_render
 def render(
        self, 
        h, 
        comp, 
-       *args
+       *args,
+       **kwargs
     ):
 
-   with self.cloudmap_resolver:
-
-      # Suppression des précédents fils
-      # dans le modèle DOM
-      self.reset_in_dom(
-              comp
-      )
+   # Suppression des précédents fils
+   # dans le modèle DOM
+   self.reset_in_dom(
+           comp
+   )
 
 
-      with h.div( 
-              class_ = 'envs_viewer %s' % ( self.aera ) 
-           ):
+   with h.div( 
+           class_ = 'envs_viewer %s' % ( self.aera ) 
+        ):
  
-         if not self.appcode:
+      if not self.appcode:
+         h << h.div( 
+                 u'Veuillez selectionner un code application', 
+                 class_ = 'appcodes message' 
+              )
+
+      elif not self.aera:
+         h << h.div( 
+                 u'Veuillez selectionner une zone', 
+                 class_ = 'aeras message' 
+              )
+
+      else:
+
+         # Initialisation locale des composants
+         # utilisés
+         self.create_all_cp_envs_viewer()
+
+         d_order = self.env_resolver.order_for_envs.copy()
+         for env, cp_env_viewer in sorted( 
+                                      self.all_cp_envs_viewer.items(), 
+                                      key = lambda e: d_order[ e[ 0 ] ], 
+                                     reverse = False
+                                   ):
+
             h << h.div( 
-                    u'Veuillez selectionner un code application', 
-                    class_ = 'appcodes message' 
-                 )
+                    component.Component( 
+                       KnownDiv( 
+                          cp_env_viewer 
+                       )
+                    ), 
+                    class_ = 'envs_viewer_struct aera %s %s' %  ( self.aera, env )
+            )
 
-         elif not self.aera:
             h << h.div( 
-                    u'Veuillez selectionner une zone', 
-                    class_ = 'aeras message' 
+                    h.div, 
+                    class_ = 'envs_viewer_struct spacer'
                  )
-
-         else:
-
-            # Initialisation locale des composants
-            # utilisés
-            self.create_all_cp_envs_viewer()
-
-            d_order = self.env_resolver.order_for_envs.copy()
-            for env, cp_env_viewer in sorted( 
-                                         self.all_cp_envs_viewer.items(), 
-                                         key = lambda e: d_order[ e[ 0 ] ], 
-                                        reverse = False
-                                      ):
-
-               h << h.div( 
-                       component.Component( 
-                          KnownDiv( 
-                             cp_env_viewer 
-                          )
-                       ), 
-                       class_ = 'envs_viewer_struct aera %s %s' %  ( self.aera, env )
-               )
-
-               h << h.div( 
-                       h.div, 
-                       class_ = 'envs_viewer_struct spacer'
-                    )
 
    return h.root
